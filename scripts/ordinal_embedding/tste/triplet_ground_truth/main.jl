7b6a675a486cb4821e2b8602fceff5ca68f8fe0a
Karel Mundnich <mundnich@usc.edu>
2017-02-17 18:26:00 -0800

    Calculating embedding and producing two signal plot

--- scripts/ordinal_embedding/tste/triplet_ground_truth/main.jl
+++ scripts/ordinal_embedding/tste/triplet_ground_truth/main.jl
@@ -3,13 +3,12 @@ include("Triplets.jl")
 using DataFrames
 using Triplets
 using NPZ
-
-# In order to use cy_tste, we need to give the right Python installation
-ENV["Python"] = "~/anaconda/bin/python"
-Pkg.build("PyCall")
 using PyCall
 @pyimport cy_tste
-@pyimport numpy as np
+
+# In order to use cy_tste, we need to give the right Python installation
+# ENV["Python"] = "~/anaconda/bin/python"
+# Pkg.build("PyCall")
 
 path = "/Users/karel/Documents/Research/BEAM/2016_continuous_annotations/scripts/ordinal_embedding/tste/triplet_ground_truth/gt_objective.csv"
 data = readtable(path)
@@ -24,11 +23,26 @@ data = data[1:30:end] # slice the data to have 1 sample/second
 
 #npzwrite("triplets.npy", trplts)
 
-fraction = 0.01
+fraction = 0.1
 total = size(trplts)[2]
 amount = convert(Int64, floor(fraction*total))
 
 subset_triplets = trplts[:,rand(1:size(trplts)[2], amount)]
 @time X = cy_tste.tste(PyReverseDims(subset_triplets), 1, 0, 2)
 
-plot(X)
+
+# Plotting
+min_value = minimum(data)
+max_value = maximum(data)
+
+if X[85] > X[100] # flip X
+    X = -X
+end
+
+X_scaled = (X-minimum(X))*(max_value-min_value)/(maximum(X)-minimum(X)) + min_value
+
+# We need to match the dimensions. Why is the dimension of the embedding 1 value
+# bigger than it should be?
+X_scaled = X_scaled[2:end,1]
+to_plot = [X_scaled data]
+plot(to_plot)
