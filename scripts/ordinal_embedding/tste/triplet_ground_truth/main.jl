c2b09f1ded7db9b0fb6be1a3ef2f463054968f82
Karel Mundnich <mundnich@usc.edu>
2017-02-18 16:59:14 -0800

    Refactoring, fixed scaling, using Plots properly

--- scripts/ordinal_embedding/tste/triplet_ground_truth/main.jl
+++ scripts/ordinal_embedding/tste/triplet_ground_truth/main.jl
@@ -4,45 +4,47 @@ using DataFrames
 using Triplets
 using NPZ
 using PyCall
+using Plots
+plotly() #Plots backend (opens plot in browser using Plotly)
 @pyimport cy_tste
 
-# In order to use cy_tste, we need to give the right Python installation
-# ENV["Python"] = "~/anaconda/bin/python"
-# Pkg.build("PyCall")
-
+# Load data and slice to have 1 sample/second
 path = "/Users/karel/Documents/Research/BEAM/2016_continuous_annotations/scripts/ordinal_embedding/tste/triplet_ground_truth/gt_objective.csv"
 data = readtable(path)
 data = data[:,1] # retrieve the column with data
 data = data[1:30:end] # slice the data to have 1 sample/second
 
-# N = 50
-# data = data[N+1:2N]
-# plot(data)
-
+# Generate triplets
 @time trplts = Triplets.generate_triplets(data)
 
-#npzwrite("triplets.npy", trplts)
-
-fraction = 0.1
+# Calculate embedding from triplets
+fraction = 0.04
 total = size(trplts)[2]
 amount = convert(Int64, floor(fraction*total))
 
 subset_triplets = trplts[:,rand(1:size(trplts)[2], amount)]
 @time X = cy_tste.tste(PyReverseDims(subset_triplets), 1, 0, 2)
+X = X[2:end,1] # Why is the embedding 268x1 instead of 267x1? Need to check
 
-
-# Plotting
+# Re-scaling
 min_value = minimum(data)
 max_value = maximum(data)
 
-if X[85] > X[100] # flip X
+if X[90] > X[100] # flip X if needed
     X = -X
 end
 
 X_scaled = (X-minimum(X))*(max_value-min_value)/(maximum(X)-minimum(X)) + min_value
 
-# We need to match the dimensions. Why is the dimension of the embedding 1 value
-# bigger than it should be?
-X_scaled = X_scaled[2:end,1]
-to_plot = [X_scaled data]
-plot(to_plot)
+# Plots
+to_plot = [data X_scaled]
+labels = Array(String, 1, 2)
+labels[1] = "Annotations"
+labels[2] = "Ground truth"
+
+plot(to_plot,
+     title = "Continuous time annotations",
+     xlabel = "Time",
+     ylabel = "Green level",
+     label = labels)
+# gui()
